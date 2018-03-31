fb531e73db1d4cc28c081e93ad32a3151c713988
Simon Bondo <simonbondo@users.noreply.github.com>
2018-03-31 22:41:57 +0200

    Added solution to the medium puzzle "Don't Panic - Episode 1"

--- training/medium/dont-panic-episode-1.cs
+++ training/medium/dont-panic-episode-1.cs
@@ -13,35 +13,123 @@ class Player
 {
     static void Main(string[] args)
     {
-        string[] inputs;
-        inputs = Console.ReadLine().Split(' ');
-        int nbFloors = int.Parse(inputs[0]); // number of floors
-        int width = int.Parse(inputs[1]); // width of the area
-        int nbRounds = int.Parse(inputs[2]); // maximum number of rounds
-        int exitFloor = int.Parse(inputs[3]); // floor on which the exit is found
-        int exitPos = int.Parse(inputs[4]); // position of the exit on its floor
-        int nbTotalClones = int.Parse(inputs[5]); // number of generated clones
-        int nbAdditionalElevators = int.Parse(inputs[6]); // ignore (always zero)
-        int nbElevators = int.Parse(inputs[7]); // number of elevators
-        for (int i = 0; i < nbElevators; i++)
-        {
-            inputs = Console.ReadLine().Split(' ');
-            int elevatorFloor = int.Parse(inputs[0]); // floor on which this elevator is found
-            int elevatorPos = int.Parse(inputs[1]); // position of the elevator on its floor
-        }
+        var area = AreaData.ReadInputs();
 
         // game loop
         while (true)
         {
-            inputs = Console.ReadLine().Split(' ');
-            int cloneFloor = int.Parse(inputs[0]); // floor of the leading clone
-            int clonePos = int.Parse(inputs[1]); // position of the leading clone on its floor
-            string direction = inputs[2]; // direction of the leading clone: LEFT or RIGHT
+            var turn = TurnData.ReadInputs();
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
+            if (turn.LeadingCloneDirection == CloneDirections.None)
+            {
+                Console.WriteLine("WAIT");
+                continue;
+            }
 
-            Console.WriteLine("WAIT"); // action: WAIT or BLOCK
+            var goalPos = turn.LeadingCloneFloor == area.ExitFloor
+                ? area.ExitPosition
+                : area.Elevators.FirstOrDefault(e => e.Floor == turn.LeadingCloneFloor)?.Position;
+            
+            if (goalPos == turn.LeadingClonePosition)
+            {
+                Console.WriteLine("WAIT");
+                continue;
+            }
+
+            var targetDirection = goalPos > turn.LeadingClonePosition
+                ? CloneDirections.Right
+                : CloneDirections.Left;
+            
+            if (turn.LeadingCloneDirection == targetDirection)
+                Console.WriteLine("WAIT");
+            else
+                Console.WriteLine("BLOCK");
         }
+    }
+
+    public class TurnData
+    {
+        // floor of the leading clone
+        public int LeadingCloneFloor { get; private set; }
+        // position of the leading clone on its floor
+        public int LeadingClonePosition { get; private set; }
+        // direction of the leading clone: LEFT or RIGHT - NONE if there are no active clones in the area
+        public CloneDirections LeadingCloneDirection { get; private set; }
+
+        public static TurnData ReadInputs()
+        {
+            var inputs = Console.ReadLine().Split(' ');
+
+            return new TurnData
+            {
+                LeadingCloneFloor = int.Parse(inputs[0]),
+                LeadingClonePosition = int.Parse(inputs[1]),
+                LeadingCloneDirection = (CloneDirections)Enum.Parse(typeof(CloneDirections), inputs[2], true)
+            };
+        }
+    }
+
+    public class AreaData
+    {
+        // 1 <= NumberOfFloors <= 15 - floors in the area
+        public int NumberOfFloors { get; private set; }
+        // 5 <= AreaWidth <= 100 - width of the area
+        public int AreaWidth { get; private set; }
+        // 10 <= MaxRounds <= 200 - maximum number of rounds
+        public int MaxRounds { get; private set; }
+        // 0 <= ExitFloor < numFloors - floor on which the exit is found
+        public int ExitFloor { get; private set; }
+        // 0 <= ExitPosition < width - position of the exit on its floor
+        public int ExitPosition { get; private set; }
+        // 2 <= NumberOfTotalClones <= 50 - number of generated clones
+        public int NumberOfTotalClones { get; private set; }
+        // ignore (always zero)
+        public int NumberOfAdditionalElevators { get; private set; }
+        // 0 <= NumberOfElevators <= 100 - number of elevators
+        public int NumberOfElevators { get; private set; }
+        public Elevator[] Elevators { get; private set; }
+
+        public static AreaData ReadInputs()
+        {
+            var inputs = Console.ReadLine().Split(' ');
+
+            var input = new AreaData
+            {
+                NumberOfFloors = int.Parse(inputs[0]),
+                AreaWidth = int.Parse(inputs[1]),
+                MaxRounds = int.Parse(inputs[2]),
+                ExitFloor = int.Parse(inputs[3]),
+                ExitPosition = int.Parse(inputs[4]),
+                NumberOfTotalClones = int.Parse(inputs[5]),
+                NumberOfAdditionalElevators = int.Parse(inputs[6]),
+                NumberOfElevators = int.Parse(inputs[7])
+            };
+
+            input.Elevators = Enumerable.Range(0, input.NumberOfElevators)
+                .Select(i => Console.ReadLine().Split(' '))
+                .Select(i => new Elevator
+                {
+                    Floor = int.Parse(i[0]),
+                    Position = int.Parse(i[1])
+                })
+                .ToArray();
+
+            return input;
+        }
+    }
+
+    public enum CloneDirections
+    {
+        None,
+        Right,
+        Left
+    }
+
+    public class Elevator
+    {
+        // 0 <= Floor < numFloors
+        public int Floor { get; set; }
+        // 0 <= Position < width
+        public int Position { get; set; }
     }
 }
