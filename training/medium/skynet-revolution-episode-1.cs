6964dc2ca17111267ca4fdde51f81db18cb3eaed
Simon Bondo <simonbondo@users.noreply.github.com>
2018-03-31 18:19:07 +0200

    Massively simplified solution to medium puzzle "Skynet Revolution - Episode 1"

--- training/medium/skynet-revolution-episode-1.cs
+++ training/medium/skynet-revolution-episode-1.cs
@@ -18,17 +18,14 @@ class Player
         // 1 <= numGateways <= 20 - the number of exit gateways
         var numGateways = int.Parse(inputs[2]);
 
-        // 0 <= node1, node2 < numNodes - node1 and node2 defines a link between these nodes
         var links = Enumerable.Range(0, numLinks)
-            .Select(l => Console.ReadLine())
-            .ToArray();
+            .Select(l => Console.ReadLine().Split(' '))
+            .Select(l => new Tuple<int, int>(int.Parse(l[0]), int.Parse(l[1])))
+            .ToList();
 
-        // 0 <= gateway <= numNodes - the index of a gateway node
         var gateways = Enumerable.Range(0, numGateways)
             .Select(e => int.Parse(Console.ReadLine()))
             .ToArray();
-
-        var network = SkynetNetwork.Create(numNodes, links, gateways);
 
         // game loop
         while (true)
@@ -36,106 +33,16 @@ class Player
             // 0 <= agentIndex <= numNodes - The index of the node on which the Skynet agent is positioned this turn
             var agentIndex = int.Parse(Console.ReadLine());
 
-            var gatewayLink = network.FindClosestGatewayLink(agentIndex);
-            network.SeverLink(gatewayLink.Item1, gatewayLink.Item2);
+            var link = links
+                // Filter only links connected to agents current node
+                .Where(l => l.Item1 == agentIndex || l.Item2 == agentIndex)
+                // Prioritise links that connect to a gateway
+                .OrderByDescending(l => gateways.Contains(l.Item1) || gateways.Contains(l.Item2))
+                .FirstOrDefault();
 
-            Console.WriteLine($"{gatewayLink.Item1} {gatewayLink.Item2}");
+            links.Remove(link);
+
+            Console.WriteLine($"{link.Item1} {link.Item2}");
         }
     }
-}
-
-public class SkynetNetwork
-{
-    public NetworkNode[] NetworkNodes { get; set; }
-
-    public NetworkNode this[int index] => this.NetworkNodes.SingleOrDefault(n => n.Id == index);
-
-    public void CreateLink(int nodeIndex1, int nodeIndex2)
-    {
-        if (nodeIndex1 == nodeIndex2)
-            return;
-
-        var node1 = this[nodeIndex1];
-        var node2 = this[nodeIndex2];
-
-        if (node1 == null || node2 == null)
-            return;
-
-        node1.Links.Add(node2);
-        node2.Links.Add(node1);
-    }
-
-    public void SeverLink(int nodeIndex1, int nodeIndex2)
-    {
-        if (nodeIndex1 == nodeIndex2)
-            return;
-
-        var node1 = this[nodeIndex1];
-        var node2 = this[nodeIndex2];
-
-        if (node1 == null || node2 == null)
-            return;
-
-        node1.Links.Remove(node2);
-        node2.Links.Remove(node1);
-    }
-
-    public Tuple<int, int> FindClosestGatewayLink(int startIndex)
-    {
-        // Find closest gateway using breadth first search
-        var visitedNodes = new List<NetworkNode>();
-        var nodeQueue = new Queue<NetworkNode>();
-
-        Func<NetworkNode, bool> unvisited = node => !visitedNodes.Contains(node);
-
-        nodeQueue.Enqueue(this[startIndex]);
-        while (nodeQueue.Count > 0)
-        {
-            var node = nodeQueue.Dequeue();
-            visitedNodes.Add(node);
-
-            // Enqueue unvisited child nodes
-            foreach (var childNode in node.Links.Where(unvisited))
-            {
-                // If we encounter a gateway, return the link from the tested node to the gateway node
-                if (childNode.IsGateway)
-                    return new Tuple<int, int>(node.Id, childNode.Id);
-
-                nodeQueue.Enqueue(childNode);
-            }
-        }
-
-        // No path leads to a gateway
-        return null;
-    }
-
-    public static SkynetNetwork Create(int numNodes, string[] links, int[] gatewayNodes)
-    {
-        // Create the network nodes
-        var network = new SkynetNetwork
-        {
-            NetworkNodes = Enumerable.Range(0, numNodes)
-                .Select(i => new NetworkNode
-                {
-                    Id = i,
-                    IsGateway = gatewayNodes.Any(e => e == i)
-                })
-                .ToArray()
-        };
-
-        // Link up each node in the network
-        foreach (var link in links.Select(l => l.Split(' ')))
-        {
-            network.CreateLink(int.Parse(link[0]), int.Parse(link[1]));
-        }
-
-        return network;
-    }
-}
-
-public class NetworkNode
-{
-    public int Id { get; set; }
-    public bool IsGateway { get; set; }
-    public List<NetworkNode> Links { get; } = new List<NetworkNode>();
 }
