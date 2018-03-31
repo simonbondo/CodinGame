9c9d8b574515a9f2063c01ac637af98fb64b0626
Simon Bondo <simonbondo@users.noreply.github.com>
2018-03-31 17:57:33 +0200

    Added solution to medium puzzle "Skynet Revolution - Episode 1"

--- training/medium/skynet-revolution-episode-1.cs
+++ training/medium/skynet-revolution-episode-1.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Linq;
-using System.IO;
-using System.Text;
-using System.Collections;
 using System.Collections.Generic;
 
 /**
@@ -13,33 +10,132 @@ class Player
 {
     static void Main(string[] args)
     {
-        string[] inputs;
-        inputs = Console.ReadLine().Split(' ');
-        int N = int.Parse(inputs[0]); // the total number of nodes in the level, including the gateways
-        int L = int.Parse(inputs[1]); // the number of links
-        int E = int.Parse(inputs[2]); // the number of exit gateways
-        for (int i = 0; i < L; i++)
-        {
-            inputs = Console.ReadLine().Split(' ');
-            int N1 = int.Parse(inputs[0]); // N1 and N2 defines a link between these nodes
-            int N2 = int.Parse(inputs[1]);
-        }
-        for (int i = 0; i < E; i++)
-        {
-            int EI = int.Parse(Console.ReadLine()); // the index of a gateway node
-        }
+        var inputs = Console.ReadLine().Split(' ');
+        // 2 <= numNodes <= 500 - the total number of nodes in the level, including the gateways
+        var numNodes = int.Parse(inputs[0]);
+        // 1 <= numLinks <= 1000 - the number of links between nodes
+        var numLinks = int.Parse(inputs[1]);
+        // 1 <= numGateways <= 20 - the number of exit gateways
+        var numGateways = int.Parse(inputs[2]);
+
+        // 0 <= node1, node2 < numNodes - node1 and node2 defines a link between these nodes
+        var links = Enumerable.Range(0, numLinks)
+            .Select(l => Console.ReadLine())
+            .ToArray();
+
+        // 0 <= gateway <= numNodes - the index of a gateway node
+        var gateways = Enumerable.Range(0, numGateways)
+            .Select(e => int.Parse(Console.ReadLine()))
+            .ToArray();
+
+        var network = SkynetNetwork.Create(numNodes, links, gateways);
 
         // game loop
         while (true)
         {
-            int SI = int.Parse(Console.ReadLine()); // The index of the node on which the Skynet agent is positioned this turn
+            // 0 <= agentIndex <= numNodes - The index of the node on which the Skynet agent is positioned this turn
+            var agentIndex = int.Parse(Console.ReadLine());
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
+            var gatewayLink = network.FindClosestGatewayLink(agentIndex);
+            network.SeverLink(gatewayLink.Item1, gatewayLink.Item2);
 
-
-            // Example: 0 1 are the indices of the nodes you wish to sever the link between
-            Console.WriteLine("0 1");
+            Console.WriteLine($"{gatewayLink.Item1} {gatewayLink.Item2}");
         }
     }
+}
+
+public class SkynetNetwork
+{
+    public NetworkNode[] NetworkNodes { get; set; }
+
+    public NetworkNode this[int index] => this.NetworkNodes.SingleOrDefault(n => n.Id == index);
+
+    public void CreateLink(int nodeIndex1, int nodeIndex2)
+    {
+        if (nodeIndex1 == nodeIndex2)
+            return;
+
+        var node1 = this[nodeIndex1];
+        var node2 = this[nodeIndex2];
+
+        if (node1 == null || node2 == null)
+            return;
+
+        node1.Links.Add(node2);
+        node2.Links.Add(node1);
+    }
+
+    public void SeverLink(int nodeIndex1, int nodeIndex2)
+    {
+        if (nodeIndex1 == nodeIndex2)
+            return;
+
+        var node1 = this[nodeIndex1];
+        var node2 = this[nodeIndex2];
+
+        if (node1 == null || node2 == null)
+            return;
+
+        node1.Links.Remove(node2);
+        node2.Links.Remove(node1);
+    }
+
+    public Tuple<int, int> FindClosestGatewayLink(int startIndex)
+    {
+        // Find closest gateway using breadth first search
+        var visitedNodes = new List<NetworkNode>();
+        var nodeQueue = new Queue<NetworkNode>();
+
+        Func<NetworkNode, bool> unvisited = node => !visitedNodes.Contains(node);
+
+        nodeQueue.Enqueue(this[startIndex]);
+        while (nodeQueue.Count > 0)
+        {
+            var node = nodeQueue.Dequeue();
+            visitedNodes.Add(node);
+
+            // Enqueue unvisited child nodes
+            foreach (var childNode in node.Links.Where(unvisited))
+            {
+                // If we encounter a gateway, return the link from the tested node to the gateway node
+                if (childNode.IsGateway)
+                    return new Tuple<int, int>(node.Id, childNode.Id);
+
+                nodeQueue.Enqueue(childNode);
+            }
+        }
+
+        // No path leads to a gateway
+        return null;
+    }
+
+    public static SkynetNetwork Create(int numNodes, string[] links, int[] gatewayNodes)
+    {
+        // Create the network nodes
+        var network = new SkynetNetwork
+        {
+            NetworkNodes = Enumerable.Range(0, numNodes)
+                .Select(i => new NetworkNode
+                {
+                    Id = i,
+                    IsGateway = gatewayNodes.Any(e => e == i)
+                })
+                .ToArray()
+        };
+
+        // Link up each node in the network
+        foreach (var link in links.Select(l => l.Split(' ')))
+        {
+            network.CreateLink(int.Parse(link[0]), int.Parse(link[1]));
+        }
+
+        return network;
+    }
+}
+
+public class NetworkNode
+{
+    public int Id { get; set; }
+    public bool IsGateway { get; set; }
+    public List<NetworkNode> Links { get; } = new List<NetworkNode>();
 }
