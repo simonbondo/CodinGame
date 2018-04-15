c061ed5c0276b58f6196750a6e045368761c3606
Simon Bondo <simonbondo@users.noreply.github.com>
2018-04-16 01:50:44 +0200

    Added initial code from medium puzzle War

---
+++ training/medium/winamax-battle.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()); // the number of cards for player 1
+        for (int i = 0; i < n; i++)
+        {
+            string cardp1 = Console.ReadLine(); // the n cards of player 1
+        }
+        int m = int.Parse(Console.ReadLine()); // the number of cards for player 2
+        for (int i = 0; i < m; i++)
+        {
+            string cardp2 = Console.ReadLine(); // the m cards of player 2
+        }
+
+        // Write an action using Console.WriteLine()
+        // To debug: Console.Error.WriteLine("Debug messages...");
+
+        Console.WriteLine("PAT");
+    }
+}
